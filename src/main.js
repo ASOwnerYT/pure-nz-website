c769f9bb1ed2c15b14bafe7d99292d72706c6d6b
John <asowneryt@archlinux-latest.thinkpad-debian>
2023-08-08 10:13:15 +1200

    Improve navbar, add more images and more

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-// Make navbar solid on scroll
+// Make navbar solid on scroll and update logo to light/dark theme
 window.addEventListener("scroll", function () {
-  var navbar = document.getElementById("navbar"); // Navbar element that will fade in
-  var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+  const navbar = document.getElementById("navbar"); // Navbar element that will fade in
+  const logo = document.getElementById("logo");
+  let scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-  // If scrolled to top, make navbar transparent, otherwise make solid
+  // If scrolled to top, make navbar transparent and update logo, otherwise make solid
   if (scrollPosition === 0) {
-    navbar.classList.remove("navbar-solid");
+    navbar.classList.remove("navbar-solid"); // Make navbar transparent
+    logo.src = "/logo.png"; // Change logo to light theme
   } else {
-    navbar.classList.add("navbar-solid");
+    navbar.classList.add("navbar-solid"); // Make navbar solid
+    logo.src = "/logo-dark.png"; // Change logo to dark theme
   }
 });
