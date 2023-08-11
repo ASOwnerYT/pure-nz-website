3a536cd0af8b9a19b058d35c2f3f4eae8df20a49
ASOwnerYT <asowner@outlook.com>
2023-08-11 09:12:26 +0000

    Improve responsiveness of main section, add spacing to navbar and other improvements

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ window.addEventListener("scroll", function () {
   // If scrolled to top, make navbar transparent and update logo, otherwise make solid
   if (scrollPosition === 0) {
     navbar.classList.remove("navbar-solid"); // Make navbar transparent
+    navbar.classList.remove("shadow"); // Shadow
     logo.src = "./public/logo.png"; // Change logo to light theme
   } else {
     navbar.classList.add("navbar-solid"); // Make navbar solid
+    navbar.classList.add("shadow"); // Shadow
     logo.src = "./public/logo-dark.png"; // Change logo to dark theme
   }
 });
