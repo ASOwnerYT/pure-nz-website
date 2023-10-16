cc22372a4427eb6238d851fa67271f1b083f4ee1
ASOwnerYT <asowner@outlook.com>
2023-10-17 11:53:51 +1300

    Replace PNG/JPG with optimized webp images

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ window.addEventListener("scroll", () => {
   if (scrollPosition === 0) {
     navbar.classList.remove("navbar-solid"); // Make navbar transparent
     navbar.classList.remove("shadow"); // Shadow
-    logo.src = "./public/logo.png"; // Change logo to light theme
+    logo.src = "./public/logo.webp"; // Change logo to light theme
   } else {
     navbar.classList.add("navbar-solid"); // Make navbar solid
     navbar.classList.add("shadow"); // Shadow
-    logo.src = "./public/logo-dark.png"; // Change logo to dark theme
+    logo.src = "./public/logo-dark.webp"; // Change logo to dark theme
   }
 });
